9ed79dc7405112e447a839cfb1d483241755f492
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2016-01-04 21:29:40 -0800

    slide fade edited

--- Script.js
+++ Script.js
@@ -1,61 +1,54 @@
 // JavaScript Document
-function btnUniv(){
-		document.getElementById("isaTxt").style.display="none";
-		document.getElementById("univTxt").style.display="block";
+function btnUniv() {
+    'use strict'; 
+    document.getElementById("isaTxt").style.display = "none";
+    document.getElementById("univTxt").style.display = "block";
+}
+function btnIsa() {
+    document.getElementById("univTxt").style.display = "none";
+    document.getElementById("isaTxt").style.display = "block";
 		
-	
-	}
-function btnIsa(){
-		document.getElementById("univTxt").style.display="none";
-		document.getElementById("isaTxt").style.display="block";
+}
+function slide() {
+    var slides = document.getElementById("myimage");
+    if (slides.src.match("col1")) {
 		
-	}
-function slide(){
-	var slides = document.getElementById("myimage");
-	if (slides.src.match("col1")) {
         slides.src = "col2.jpg";
-		$("#myimage").fadeOut()
-		 $("#myimage").fadeIn()
-    } 
-	else if (slides.src.match("col2")) {
+		$("#myimage").fadeIn()
+    } else if (slides.src.match("col2")) {
         slides.src = "col3.jpg";
-		$("#myimage").fadeOut()
-		 $("#myimage").fadeIn()
+		$("#myimage").fadeIn()
     } else {
+		
         slides.src = "col1.jpg";
-		$("#myimage").fadeOut()
-		 $("#myimage").fadeIn()
-    }
-	/*for(var i=1; i<3;i++){
-		slides.style.backgroundImage="url(col1.jpg)";
-		}*/
-		setTimeout(slide, 5000);
+		$("#myimage").fadeIn()
+    } 
+    
+	setTimeout(slide, 5000);
+	$("#myimage").fadeOut(5000)
 	}
 slide();
 
-function change(getId){
+function change(getId) {
 	var idToChange = document.getElementById(getId);
-	var slides = document.getElementById("myimage");
-	if(getId=="changeImgPrev"){
-	if (slides.src.match("col1")) {
+    var slides = document.getElementById("myimage");
+	if (getId == "changeImgPrev") {
+	if (slides.src.match("col1") ) {
         slides.src = "col3.jpg";
-    } 
-	else if (slides.src.match("col2")) {
+    } else if (slides.src.match("col2")) {
         slides.src = "col1.jpg";
     } else {
         slides.src = "col2.jpg";
     }
 	}
-	if(getId=="changeImgNext"){
+	if (getId == "changeImgNext") {
 	if (slides.src.match("col1")) {
-        
 		/*$("#myimage").animate({
           height: 'toggle'
         },"slow");*/
 		$("#myimage").animate({width: '100%', opacity: '1'}, "slow");
-    slides.src = "col2.jpg";
-	} 
-	else if (slides.src.match("col2")) {
+        slides.src = "col2.jpg";
+    } else if (slides.src.match("col2")) {
 		/*$("#myimage").animate({
           height: 'toggle'
         },"slow");
@@ -63,7 +56,7 @@ function change(getId){
           height: 'toggle'
         },"slow");*/
         slides.src = "col3.jpg";
-    } else { 
+        } else { 
 		/*$("#myimage").animate({
           height: 'toggle'
         },"slow");
